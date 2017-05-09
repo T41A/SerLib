025f1d9a708703b74e7643b6f7fb6a1964a2a08d
Vidicon <bram31f@gmail.com>
2017-05-09 10:28:56 +0200

    Add files via upload

---
+++ serLCD/examples/SerLcdExample/SerLcdExample.ino
@@ -0,0 +1,17 @@
+/*
+  serLCD - Hello World Example
+*/
+#include <NewSoftSerial.h>
+#include <serLCD.h>
+ 
+// Set pin to the LCD's rxPin
+int pin = 2;
+ 
+serLCD lcd(pin);
+ 
+void setup()
+{
+  lcd.print("hello, world!");
+}
+ 
+void loop() {}
